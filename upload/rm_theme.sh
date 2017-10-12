29f35a586ebfe2cd634740cb27d7b502daee178e
Serg Ryabinin <scanner85@gmail.com>
2017-10-12 10:30:39 +0300

    to default theme

---
+++ upload/rm_theme.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -f ./admin/language/en-gb/extension/theme/theme2.php
+rm -f ./admin/view/template/extension/theme/theme2.twig
+rm -f touch ./admin/controller/extension/theme/theme2.php
---
+++ upload/set_theme.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+touch ./admin/language/en-gb/extension/theme/theme2.php
+touch ./admin/view/template/extension/theme/theme2.twig
+touch ./admin/controller/extension/theme/theme2.php
