5c46e71ba0b406b4c3d8b898f3aed5efac7ab727
Serg Ryabinin <scanner85@gmail.com>
2017-10-14 20:25:25 +0300

    draft some pages

--- upload/catalog/controller/common/menu_top.php
+++ upload/catalog/controller/common/menu_top.php
@@ -10,11 +10,13 @@ class ControllerCommonMenuTop extends Controller {
 		];
 
 		$data['down_line']= [ 
-			['title'=>'TITLE_21', 'href' => '#1', 'target'=>'_self', 'active' => true],
-			['title'=>'TITLE_22', 'href' => '#2', 'target'=>'_self', 'active' => true],
-			['title'=>'TITLE_23', 'href' => '#3', 'target'=>'_blank', 'active' => true],
-			['title'=>'TITLE_24', 'href' => '#3', 'target'=>'_blank', 'active' => true],
-			['title'=>'TITLE_25', 'href' => '#3', 'target'=>'_blank', 'active' => true], 
+			['title'=>'ПРОДУКЦИЯ', 'href' => '#1', 'target'=>'_self', 'active' => true],
+			['title'=>'О КОМПАНИИ', 'href' => '#2', 'target'=>'_self', 'active' => true],
+			['title'=>'ДИЛЕРАМ', 'href' => '#3', 'target'=>'_blank', 'active' => true],
+			['title'=>'ПУБЛИКАЦИИ', 'href' => '#3', 'target'=>'_blank', 'active' => true],
+			['title'=>'НОВОСТИ', 'href' => '#3', 'target'=>'_blank', 'active' => true], 			
+			['title'=>'ОБРАТНАЯ СВЯЗЬ', 'href' => '#3', 'target'=>'_blank', 'active' => true],
+			['title'=>'TITLE_25', 'href' => '#3', 'target'=>'_blank', 'active' => false], 
 		];
 
 		return $this->load->view('common/menu_top', $data);
