c801aae3a54978f6b6b1f8c21134f011b06f328a
Serg Ryabinin <scanner85@gmail.com>
2017-11-15 05:20:46 +0300

    ONENEWS page

---
+++ upload/catalog/language/en-gb/information/onenews.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']  = 'One news';
+$_['heading_title_pub'] = 'Related news';
+// Text
+$_['text_location']  = 'Our Location';
+$_['text_store']     = 'Our Stores';
+$_['text_contact']   = 'Contact Form';
+$_['text_address']   = 'Address';
+$_['text_telephone'] = 'Telephone';
+$_['text_fax']       = 'Fax';
+$_['text_open']      = 'Opening Times';
+$_['text_comment']   = 'Comments';
+$_['text_message']   = '<p>Your enquiry has been successfully sent to the store owner!</p>';
+$_['button_submit']  = 'Submit';
+$_['button_more']    = 'More';
+$_['text_error']	= 'While news articles not present on site. Coming soon.';
+
+// Entry
+$_['entry_name']     = 'Your Name';
+$_['entry_email']    = 'E-Mail Address';
+$_['entry_enquiry']  = 'Enquiry';
+
+// Email
+$_['email_subject']  = 'Enquiry %s';
+
+// Errors
+$_['error_name']     = 'Name must be between 3 and 32 characters!';
+$_['error_email']    = 'E-Mail Address does not appear to be valid!';
+$_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters!';
