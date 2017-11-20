ce8dda230d44c6791a057a697beb51907b83e910
Serg Ryabinin <scanner85@gmail.com>
2017-11-21 00:57:36 +0300

    compacted menu

--- upload/catalog/controller/common/menu_top.php
+++ upload/catalog/controller/common/menu_top.php
@@ -15,18 +15,18 @@ class ControllerCommonMenuTop extends Controller {
 		//var_dump($data['lang']); die();
 		if( $data['lang'] == 'ru'):
 			$data['up_line']= [ 
-				['class'=>'menu-bold', 'title'=>'8 800 200 30 35', 'awesome' => 'fa fa-phone', 'img' => '', 'href' => '#1', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-space menu-bold', 'title'=>'ПОДБОР ПРОДУКТА', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-space menu-bold', 'title'=>'ДЕЗКАЛЬКУЛЯТОР' , 'awesome' => 'fa fa-calculator', 'img' => '', 'href' => 'index.php?route=information/dezcalc', 'target'=>'_blank', 'active' => true], 
+				['class'=>'menu-bold ', 'title'=>'8 800 200 30 35', 'awesome' => 'fa fa-phone', 'img' => '', 'href' => '#1', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-space menu-bold ' , 'title'=>'ПОДБОР ПРОДУКТА', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-space menu-bold ', 'title'=>'ДЕЗКАЛЬКУЛЯТОР' , 'awesome' => 'fa fa-calculator', 'img' => '', 'href' => 'index.php?route=information/dezcalc', 'target'=>'_blank', 'active' => true], 
 			];
 
 			$data['down_line']= [ 
-				['class'=>'', 'title'=>'ПРОДУКЦИЯ', 'href' => 'index.php?route=product/category&path=10', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-bold', 'title'=>'О КОМПАНИИ', 'href' => 'index.php?route=information/about', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-bold', 'title'=>'ДИЛЕРАМ', 'href' => 'index.php?route=information/information&information_id=10', 'target'=>'_blank', 'active' => true],
-				['class'=>'menu-bold', 'title'=>'ПУБЛИКАЦИИ', 'href' => 'index.php?route=information/articlelist', 'target'=>'_blank', 'active' => true],
-				['class'=>'menu-bold', 'title'=>'НОВОСТИ', 'href' => 'index.php?route=information/newslist', 'target'=>'_blank', 'active' => true], 			
-				['class'=>'menu-bold', 'title'=>'ОБРАТНАЯ СВЯЗЬ', 'href' => 'index.php?route=information/contact', 'target'=>'_blank', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'ПРОДУКЦИЯ', 'href' => 'index.php?route=product/category&path=10', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'О КОМПАНИИ', 'href' => 'index.php?route=information/about', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'ДИЛЕРАМ', 'href' => 'index.php?route=information/information&information_id=10', 'target'=>'_blank', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'ПУБЛИКАЦИИ', 'href' => 'index.php?route=information/articlelist', 'target'=>'_blank', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'НОВОСТИ', 'href' => 'index.php?route=information/newslist', 'target'=>'_blank', 'active' => true], 			
+				['class'=>'menu-bold ', 'title'=>'ОБРАТНАЯ СВЯЗЬ', 'href' => 'index.php?route=information/contact', 'target'=>'_blank', 'active' => true],
 				//['title'=>'TITLE_25', 'href' => '#3', 'target'=>'_blank', 'active' => false], 
 			];
 			$data['apply_menu_list'] = [ 
@@ -38,19 +38,19 @@ class ControllerCommonMenuTop extends Controller {
 
 		if( $data['lang'] == 'en'):
 			$data['up_line']= [ 
-				['class'=>'menu-bold', 'title'=>'8 800 200 30 35', 'awesome' => 'fa fa-phone',  'img' => '', 'href' => '#1', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-space menu-bold', 'title'=>'PRODUCT SELECTION', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-space menu-bold', 'title'=>'DEZ CALCULATOR', 'awesome' => 'fa fa-calculator','img' => '', 
+				['class'=>'menu-bold ', 'title'=>'8 800 200 30 35', 'awesome' => 'fa fa-phone',  'img' => '', 'href' => '#1', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-space menu-bold ', 'title'=>'PRODUCT SELECTION', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-space menu-bold ', 'title'=>'DEZ CALCULATOR', 'awesome' => 'fa fa-calculator','img' => '', 
 				'href' => 'index.php?route=information/dezcalc', 'target'=>'_blank', 'active' => true], 
 			];
 
 			$data['down_line']= [ 
-				['class'=>'menu-bold', 'title'=>'PRODUCTS', 'href' => 'index.php?route=product/category&path=10', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-bold', 'title'=>'ABOUT', 'href' => 'index.php?route=information/about', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-bold', 'title'=>'DEALERS', 'href' => 'index.php?route=information/information&information_id=10', 'target'=>'_blank', 'active' => true],
-				['class'=>'menu-bold', 'title'=>'ARTICLES', 'href' => 'index.php?route=information/articlelist', 'target'=>'_blank', 'active' => true],
-				['class'=>'menu-bold', 'title'=>'NEWS', 'href' => 'index.php?route=information/newslist', 'target'=>'_blank', 'active' => true], 			
-				['class'=>'menu-bold', 'title'=>'FEEDBACK', 'href' => 'index.php?route=information/contact', 'target'=>'_blank', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'PRODUCTS', 'href' => 'index.php?route=product/category&path=10', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'ABOUT', 'href' => 'index.php?route=information/about', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'DEALERS', 'href' => 'index.php?route=information/information&information_id=10', 'target'=>'_blank', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'ARTICLES', 'href' => 'index.php?route=information/articlelist', 'target'=>'_blank', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'NEWS', 'href' => 'index.php?route=information/newslist', 'target'=>'_blank', 'active' => true], 			
+				['class'=>'menu-bold ', 'title'=>'FEEDBACK', 'href' => 'index.php?route=information/contact', 'target'=>'_blank', 'active' => true],
 				//['title'=>'TITLE_25', 'href' => '#3', 'target'=>'_blank', 'active' => false], 
 			];
 			$data['apply_menu_list'] = [ 
--- upload/catalog/language/en-gb/extension/module/featured.php
+++ upload/catalog/language/en-gb/extension/module/featured.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'Professional manufacturer of Cheme';
+$_['heading_title'] = 'Our products';
 
 // Text
 $_['text_tax']      = 'Ex Tax:';
