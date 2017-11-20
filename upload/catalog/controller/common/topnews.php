260731b09033382906284e2eaf2ca3873bbcfac2
Serg Ryabinin <scanner85@gmail.com>
2017-11-20 12:34:10 +0300

    top menu fix

---
+++ upload/catalog/controller/common/topnews.php
@@ -0,0 +1,41 @@
+<?php
+class ControllerCommonTopnews extends Controller {
+	public function index( $aOption=[]) {
+		$this->load->language('common/toparticles');
+
+		$this->load->model('catalog/information');
+		if( array_key_exists('news', $aOption) ){
+			$iLimit = $aOption['news'];
+		} else {
+			$iLimit = 6;
+		}
+		$aTopArticles = $this->model_catalog_information->getTopNews( $iLimit );
+		/* get first image for each news */
+		$aResult = [];
+		foreach ($aTopArticles as $key => $value) {
+			$aData = [];
+
+			if(is_array($value)){
+				foreach ($value as $key2 => $value2) {
+				
+					$content = html_entity_decode( $value2 );
+					$content = preg_replace("/<img[^>]+\>/i", "", $content); 
+					$aOut = [];
+
+					if( $key2 == 'description'){
+						if( preg_match_all("/<img[^>]+\>/i", html_entity_decode( $value2 ), $aOut) ){
+							$aData[ 'image' ] = $aOut[0][0];//первая картинка 	
+						}
+					}
+
+					$aData[$key2] = html_entity_decode( $content );
+				}
+				$aResult[] = $aData;
+			}
+		}
+		$data['topnews'] = $aResult;
+		//var_dump($data); die();
+
+		return $this->load->view('common/topnews', $data);
+	}
+}
