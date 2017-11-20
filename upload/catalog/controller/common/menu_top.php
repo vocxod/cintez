bf3eafa8be551296cb9ec734121f00341f29e183
Serg Ryabinin <scanner85@gmail.com>
2017-11-20 16:03:34 +0300

    fix menu view (caps)

--- upload/catalog/controller/common/menu_top.php
+++ upload/catalog/controller/common/menu_top.php
@@ -16,8 +16,8 @@ class ControllerCommonMenuTop extends Controller {
 		if( $data['lang'] == 'ru'):
 			$data['up_line']= [ 
 				['class'=>'', 'title'=>'8 800 200 30 35', 'awesome' => 'fa fa-phone', 'img' => '', 'href' => '#1', 'target'=>'_self', 'active' => true],
-				['class'=>'', 'title'=>'ПОДБОР ПРОДУКТА', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true],
-				['class'=>'', 'title'=>'ДЕЗКАЛЬКУЛЯТОР' , 'awesome' => 'fa fa-calculator', 'img' => '', 'href' => 'index.php?route=information/dezcalc', 'target'=>'_blank', 'active' => true], 
+				['class'=>'menu-space', 'title'=>'ПОДБОР ПРОДУКТА', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-space', 'title'=>'ДЕЗКАЛЬКУЛЯТОР' , 'awesome' => 'fa fa-calculator', 'img' => '', 'href' => 'index.php?route=information/dezcalc', 'target'=>'_blank', 'active' => true], 
 			];
 
 			$data['down_line']= [ 
@@ -39,8 +39,8 @@ class ControllerCommonMenuTop extends Controller {
 		if( $data['lang'] == 'en'):
 			$data['up_line']= [ 
 				['class'=>'', 'title'=>'8 800 200 30 35', 'awesome' => 'fa fa-phone',  'img' => '', 'href' => '#1', 'target'=>'_self', 'active' => true],
-				['class'=>'', 'title'=>'PRODUCT SELECTION', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true],
-				['class'=>'', 'title'=>'DEZ CALCULATOR', 'awesome' => 'fa fa-calculator','img' => '', 
+				['class'=>'menu-space', 'title'=>'PRODUCT SELECTION', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-space', 'title'=>'DEZ CALCULATOR', 'awesome' => 'fa fa-calculator','img' => '', 
 				'href' => 'index.php?route=information/dezcalc', 'target'=>'_blank', 'active' => true], 
 			];
 
