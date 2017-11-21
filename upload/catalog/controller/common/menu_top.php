6aa71b9334961109fc7587b5bee42cac834a1d8c
Serg Ryabinin <scanner85@gmail.com>
2017-11-21 05:07:36 +0300

    set selected class for menu

--- upload/catalog/controller/common/menu_top.php
+++ upload/catalog/controller/common/menu_top.php
@@ -8,25 +8,28 @@ class ControllerCommonMenuTop extends Controller {
 		$this->load->model('design/banner');
 
 		$aBanner= $this->model_design_banner->getBanner( 9 );
-		//var_dump( $aBanner ); die();
-		$data['banners'] = $aBanner;
- 		
+		$data['banners'] = $aBanner;		
 		$data['lang'] = $this->language->get('code');
-		//var_dump($data['lang']); die();
+		
 		if( $data['lang'] == 'ru'):
 			$data['up_line']= [ 
-				['class'=>'menu-bold ', 'title'=>'8 800 200 30 35', 'awesome' => 'fa fa-phone', 'img' => '', 'href' => '#1', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-space menu-bold ' , 'title'=>'ПОДБОР ПРОДУКТА', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-space menu-bold ', 'title'=>'ДЕЗКАЛЬКУЛЯТОР' , 'awesome' => 'fa fa-calculator', 'img' => '', 'href' => 'index.php?route=information/dezcalc', 'target'=>'_blank', 'active' => true], 
+				['class'=>'menu-bold ', 'title'=>'8 800 200 30 35', 'awesome' => 'fa fa-phone', 'img' => '', 'href' => '#1', 'target'=>'_self', 'active' => true, 'route'=>''],
+				['class'=>'menu-space menu-bold ' , 'title'=>'ПОДБОР ПРОДУКТА', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true, 'route'=>'information/productselection'],
+				['class'=>'menu-space menu-bold ', 'title'=>'ДЕЗКАЛЬКУЛЯТОР' , 'awesome' => 'fa fa-calculator', 'img' => '', 'href' => 'index.php?route=information/dezcalc', 'target'=>'_blank', 'active' => true, 'route' => 'information/dezcalc'], 
 			];
 
 			$data['down_line']= [ 
-				['class'=>'menu-bold ', 'title'=>'ПРОДУКЦИЯ', 'href' => 'index.php?route=product/category&path=10', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-bold ', 'title'=>'О КОМПАНИИ', 'href' => 'index.php?route=information/about', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-bold ', 'title'=>'ДИЛЕРАМ', 'href' => 'index.php?route=information/information&information_id=10', 'target'=>'_blank', 'active' => true],
-				['class'=>'menu-bold ', 'title'=>'ПУБЛИКАЦИИ', 'href' => 'index.php?route=information/articlelist', 'target'=>'_blank', 'active' => true],
-				['class'=>'menu-bold ', 'title'=>'НОВОСТИ', 'href' => 'index.php?route=information/newslist', 'target'=>'_blank', 'active' => true], 			
-				['class'=>'menu-bold ', 'title'=>'ОБРАТНАЯ СВЯЗЬ', 'href' => 'index.php?route=information/contact', 'target'=>'_blank', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'ПРОДУКЦИЯ', 'href' => 'index.php?route=product/category&path=10', 'target'=>'_self', 'active' => true, 'route' => ''],
+				
+				['class'=>'menu-bold ', 'title'=>'О КОМПАНИИ', 'href' => 'index.php?route=information/about', 'target'=>'_self', 'active' => true, 'route' => 'information/about'],
+
+				['class'=>'menu-bold ', 'title'=>'ДИЛЕРАМ', 'href' => 'index.php?route=information/information&information_id=10', 'target'=>'_blank', 'active' => true, 'route' => ''],
+				
+				['class'=>'menu-bold ', 'title'=>'ПУБЛИКАЦИИ', 'href' => 'index.php?route=information/articlelist', 'target'=>'_blank', 'active' => true, 'route' => 'information/articlelist'],
+				
+				['class'=>'menu-bold ', 'title'=>'НОВОСТИ', 'href' => 'index.php?route=information/newslist', 'target'=>'_blank', 'active' => true, 'route' => 'information/newslist'], 			
+				
+				['class'=>'menu-bold ', 'title'=>'ОБРАТНАЯ СВЯЗЬ', 'href' => 'index.php?route=information/contact', 'target'=>'_blank', 'active' => true, 'route' => 'information/contact'],
 				//['title'=>'TITLE_25', 'href' => '#3', 'target'=>'_blank', 'active' => false], 
 			];
 			$data['apply_menu_list'] = [ 
@@ -38,19 +41,26 @@ class ControllerCommonMenuTop extends Controller {
 
 		if( $data['lang'] == 'en'):
 			$data['up_line']= [ 
-				['class'=>'menu-bold ', 'title'=>'8 800 200 30 35', 'awesome' => 'fa fa-phone',  'img' => '', 'href' => '#1', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-space menu-bold ', 'title'=>'PRODUCT SELECTION', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'8 800 200 30 35', 'awesome' => 'fa fa-phone',  'img' => '', 'href' => '#1', 'target'=>'_self', 'active' => true, 'route' => ''],
+
+				['class'=>'menu-space menu-bold ', 'title'=>'PRODUCT SELECTION', 'awesome' => 'fa fa-binoculars', 'img' => '', 'href' => 'index.php?route=information/productselection', 'target'=>'_self', 'active' => true, 'route'=>'information/productselection'],
+				
 				['class'=>'menu-space menu-bold ', 'title'=>'DEZ CALCULATOR', 'awesome' => 'fa fa-calculator','img' => '', 
-				'href' => 'index.php?route=information/dezcalc', 'target'=>'_blank', 'active' => true], 
+				'href' => 'index.php?route=information/dezcalc', 'target'=>'_blank', 'active' => true, 'route'=>'information/dezcalc'], 
 			];
 
 			$data['down_line']= [ 
-				['class'=>'menu-bold ', 'title'=>'PRODUCTS', 'href' => 'index.php?route=product/category&path=10', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-bold ', 'title'=>'ABOUT', 'href' => 'index.php?route=information/about', 'target'=>'_self', 'active' => true],
-				['class'=>'menu-bold ', 'title'=>'DEALERS', 'href' => 'index.php?route=information/information&information_id=10', 'target'=>'_blank', 'active' => true],
-				['class'=>'menu-bold ', 'title'=>'ARTICLES', 'href' => 'index.php?route=information/articlelist', 'target'=>'_blank', 'active' => true],
-				['class'=>'menu-bold ', 'title'=>'NEWS', 'href' => 'index.php?route=information/newslist', 'target'=>'_blank', 'active' => true], 			
-				['class'=>'menu-bold ', 'title'=>'FEEDBACK', 'href' => 'index.php?route=information/contact', 'target'=>'_blank', 'active' => true],
+				['class'=>'menu-bold ', 'title'=>'PRODUCTS', 'href' => 'index.php?route=product/category&path=10', 'target'=>'_self', 'active' => true, 'route' => ''],
+				
+				['class'=>'menu-bold ', 'title'=>'ABOUT', 'href' => 'index.php?route=information/about', 'target'=>'_self', 'active' => true, 'route' => 'information/about'],
+				
+				['class'=>'menu-bold ', 'title'=>'DEALERS', 'href' => 'index.php?route=information/information&information_id=10', 'target'=>'_blank', 'active' => true, 'route' => ''],
+				
+				['class'=>'menu-bold ', 'title'=>'ARTICLES', 'href' => 'index.php?route=information/articlelist', 'target'=>'_blank', 'active' => true, 'route' => 'information/articlelist'],
+				
+				['class'=>'menu-bold ', 'title'=>'NEWS', 'href' => 'index.php?route=information/newslist', 'target'=>'_blank', 'active' => true, 'route' => 'information/newslist'], 			
+
+				['class'=>'menu-bold ', 'title'=>'FEEDBACK', 'href' => 'index.php?route=information/contact', 'target'=>'_blank', 'active' => true, 'route' => 'information/contact'],
 				//['title'=>'TITLE_25', 'href' => '#3', 'target'=>'_blank', 'active' => false], 
 			];
 			$data['apply_menu_list'] = [ 
@@ -97,10 +107,34 @@ class ControllerCommonMenuTop extends Controller {
 				]  ],	
 		];
 
-		
-
-
-		//var_dump( $data['apply_menu_list'] ); die();
+		if (isset($this->request->get['route'])) {
+			$sRoute = (string)$this->request->get['route'];
+			// найти текущий пункт меню, добавить класс
+			if( $sRoute != '' ){
+				$data['lang'] = $this->language->get('code');
+				foreach ($data['up_line'] as $iKey => $aItem) {
+					foreach ($aItem as $sKey => $sMenu) {
+						if( $sKey =='route'){
+							if( $sMenu == $sRoute ){
+								$data['up_line'][$iKey]['class'] .= " selected-item";
+							}
+						}
+					}
+				}
+				foreach ($data['down_line'] as $iKey => $aItem) {
+					foreach ($aItem as $sKey => $sMenu) {
+						if( $sKey =='route'){
+							if( $sMenu == $sRoute ){
+								//var_dump("found");
+								$data['down_line'][$iKey]['class'] .= " selected-item";
+							}
+						}
+					}
+				}
+			}
+    	}
+    	//var_dump($sRoute);
+		//var_dump( $data['down_line'] ); //die();
 		return $this->load->view('common/menu_top', $data);
 	}
 }
