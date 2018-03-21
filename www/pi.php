bff2b422e2a5f7788793eada062611852d2eaaf6
root <root@debian-9-x64.static.spheral.ru>
2018-03-21 12:27:56 +0300

    disable prod wo images

--- www/pi.php
+++ www/pi.php
@@ -2,6 +2,7 @@
 
 echo getcwd();
 
+echo date("Y-m-d H:i:s", time()) . "\n";
 phpinfo();
 
 
--- www/set_theme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-touch ./admin/language/en-gb/extension/theme/theme2.php
-touch ./admin/view/template/extension/theme/theme2.twig
-touch ./admin/controller/extension/theme/theme2.php
