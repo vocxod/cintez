9af5c5c3f42c1bce0c0ecf82f70aee26fd5553c6
scard <scanner85@gmail.com>
2018-04-03 16:46:07 +0300

    return_menu

--- www/catalog/language/en-gb/account/login.php
+++ www/catalog/language/en-gb/account/login.php
@@ -5,7 +5,7 @@ $_['heading_title']                = 'Account Login';
 // Text
 $_['text_account']                 = 'Account';
 $_['text_login']                   = 'Login';
-$_['text_new_customer']            = 'New Customer';
+$_['text_new_customer']            = 'New User';
 $_['text_register']                = 'Register Account';
 $_['text_register_account']        = 'By creating an account you will be able to shop faster, be up to date on an order\'s status, and keep track of the orders you have previously made.';
 $_['text_returning_customer']      = 'Returning Customer';
