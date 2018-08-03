8100dab74bd87aad5c8fa20c7623d39f786e23f6
scard <scanner85@gmail.com>
2018-08-03 18:27:11 +0300

    sitemap

--- cli_utils/sitemap.php
+++ cli_utils/sitemap.php
@@ -17,18 +17,26 @@ class Sitemap {
 	public $s_result = '';
 
 	public function run(){
-		
-		$a_seo = OcSeoUrlQuery::create()
-		//->filterByQuery( "product_id%" )
-		->filterByLanguageId(4)
-		->where('oc_seo_url.query LIKE ?', 'product_id=%')
-		//->toString();
-		->find();
+
 		$s_url = '';
-		foreach ($a_seo as $obj) {
-			$s_url .= '<url><loc>https://www.specsintez.com/' . $obj->getKeyword() . '</loc><changefreq>weekly</changefreq><priority>0.7</priority></url>';
-		}	
+
+		$a_products = OcProductQuery::create()
+		->filterByStatus(1)
+		->find();
+		foreach( $a_products as $a_product ){
+
+			$a_seo = OcSeoUrlQuery::create()
+			->filterByQuery( "product_id=" . $a_product->getProductId() )
+			->filterByLanguageId(4)
+			->findOne();
+			if( $a_seo != null){
+				$s_url .= '<url><loc>https://www.specsintez.com/' . $a_seo->getKeyword() . '</loc><changefreq>weekly</changefreq><priority>0.7</priority></url>';
+			}
+
+		}
+		
 		$s_result = '<urlset>' . $s_url . '</urlset>';
+
 		header("Content-Type: text/xml");
 		header("Expires: Thu, 19 Feb 1998 13:24:18 GMT");
 		header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
