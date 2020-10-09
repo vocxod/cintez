81f93ff5b86df9e30b6691976ffd2756cbdeb1f2
Your Name <you@example.com>
2020-10-09 12:07:16 +0300

    images

--- www/promo/presentational-only/presentational-only.js
+++ www/promo/presentational-only/presentational-only.js
@@ -9,7 +9,7 @@ $(document).ready(function()
 
     if( $('#more-content').hasClass('shown') )
     {
-      $('#load-more-content').text('Hide content');
+      $('#load-more-content').text('С к р ы т ь');
       $('#more-content').fadeIn('slow', function()
       {
         $('#load-more-content').fadeIn('slow');
@@ -17,7 +17,7 @@ $(document).ready(function()
     }
     else
     {
-      $('#load-more-content').text('Load some content');
+      $('#load-more-content').text('П о к а з а т ь');
       $('#more-content').fadeOut('slow', function()
       {
         $('#load-more-content').fadeIn('slow');
