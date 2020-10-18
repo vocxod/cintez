89e31842dc5b3bd5c8eea15993bdd4a55ac73218
Your Name <you@example.com>
2020-10-18 19:09:08 +0300

    index4

---
+++ www/promo/index2.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Спецсинтез - проко акции</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="author" content="SpecSintez.com">
+  <meta name="description" content="Акции компании Спецсинтез">
+  <link rel="icon" href="http://sixrevisions.com/favicon.ico" type="image/x-icon" />
+  <link href="http://fonts.googleapis.com/css?family=Kotta+One|Cantarell:400,700" rel="stylesheet" type="text/css">
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <link rel="stylesheet" href="presentational-only/presentational-only.css?v=123">
+  <link rel="stylesheet" href="responsive-full-background-image.css">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="presentational-only/presentational-only.js"></script>
+</head>
+<body>
+
+<?php
+
+?>
+
+  <!--
+<div class="row_header">
+  <div class="col-left">
+    <img src="images/sslogo.png">
+    <p>
+      Разработка и производство профессиональных<br/>
+      дезинфицирующих и моющих средств с 2004 года.
+    </p>
+  </div>
+  <div class="col-right">
+    <p>8-812-416-47-17</p>
+    <p>zakaz@specsintez.com</p>
+  </div>
+</div>
+-->
+
+<div id="container">
+  <div class="col-left">
+  <img src="images/sslogo.png">
+    <p>
+      Разработка и производство профессиональных<br/>
+      дезинфицирующих и моющих средств с 2004 года.
+    </p>    
+  </div>
+
+  <div class="col-center">
+    
+  </div>
+  
+  <div class="col-right">
+      <p class="p1">8-812-416-47-17</p>
+      <p class="p2">zakaz@specsintez.com</p>
+  </div>
+</div>
+
+
+<div id="container2">
+  <div class="col-left2">
+
+    <div class="black_form">
+      <p>
+        Для заказа продукции <br/>
+        оставьте заявку:
+      </p>
+      <br/>
+
+      <div style="text-align: left; width:80%; margin: 0 auto;">
+        <label class="label" for="s_name">Имя:</label>
+        <br/>
+        <input class="input" type="text" name="s_name" id="s_name" placeholder="Как к вам обращаться?">
+        </p>
+        <p>
+        <br/>
+
+        <label class="label" for="s_phone">Телефон:</label>
+        <br/>
+        <input class="input" type="text" name="s_phone" id="s_phone" placeholder="Ваш телефон">
+        </p>
+        <br/>
+
+        <p>
+        <label class="label" for="s_email">
+          Адрес электронной почты:
+        </label>
+        <br/>
+        <input class="input" type="text" name="s_email" id="s_email" placeholder="Ваш email">
+        </p>
+        <br/>
+
+        <p>
+        <br/>
+        <input type="checkbox" name="s_access" id=s_access> <span style="font-size: 16px;">Согласен на обработку персональных данных.</span>
+        </p>
+        <br/>
+
+        <p style="text-align: center;">
+        <input type="submit" name="s_submit" value="Отправить">
+        </p>
+        <br/>
+      </div>
+      
+    <form>
+      
+    </form>
+    </div>
+
+  </div>
+
+  <div class="col-center2">
+    <img src="images/preparat_1.png"/>
+  </div>
+  
+  <div class="col-right2">
+    <img src="images/preparat_2.png"/>
+  </div>
+
+</div>
+
+  <nav class="navbar" id="top">
+    <div class="inner">
+      <a href="https://specsintez.com/katalog">Перейти на сайт производителя</a>
+    </div>
+  </nav>
+
+
+<!--
+  <header class="container">
+    <section class="content">
+      <h1>СПЕЦСИНТЕЗ</h1>
+      <p class="sub-title"><strong>Получите</strong> <br /> наши последние предложения</p>
+      <p></p>
+      <div id="more-content-main">
+        <blockquote>
+        <img class="promo-image" src="images/trioseptaqua.png"/>
+        <cite>
+          Триосепт Аква<br/>
+          Описание на нашем сайте:<a href="https://specsintez.com/%D1%82%D1%80%D0%B8%D0%BE%D1%81%D0%B5%D0%BF%D1%82-%D0%B0%D0%BA%D0%B2%D0%B0" target="_blank">Триосепт Аква</a>
+        </cite>
+        </blockquote>
+
+        <blockquote>
+        <img class="promo-image" src="images/milasept.png"/>
+        <cite>
+          МилАсепт
+          Описание на нашем сайте:<a href="https://specsintez.com/index.php?route=product/product&product_id=1029" target="_blank">МилАсепт</a>
+        </cite>
+        </blockquote>
+
+        <blockquote>
+        <img class="promo-image" src="images/nevaseptlong.png"/>
+        <cite>
+          Невасепт Лонг
+          Описание на нашем сайте:<a href="https://specsintez.com/index.php?route=product/product&product_id=1036" target="_blank">Невасепт Лонг</a>
+        </cite>
+        </blockquote>
+
+        <blockquote>
+        <img class="promo-image" src="images/trioseptexpress.png"/>
+        <cite>
+          Триосепт Экспресс
+          Описание на нашем сайте:<a href="https://specsintez.com/%D1%82%D1%80%D0%B8%D0%BE%D1%81%D0%B5%D0%BF%D1%82-%D1%8D%D0%BA%D1%81%D0%BF%D1%80%D0%B5%D1%81%D1%81" target="_blank">Триосепт Экспресс</a>
+        </cite>
+        </blockquote>
+      </div>
+    </section>
+  </header>
+-->
+
+</body>
+</html>
