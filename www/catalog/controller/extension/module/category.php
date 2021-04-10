2e0591365d536b4155d556f615b0cacdf76586e6
Your Name <you@example.com>
2021-04-10 18:50:03 +0300

    last category sjow

--- www/catalog/controller/extension/module/category.php
+++ www/catalog/controller/extension/module/category.php
@@ -25,7 +25,7 @@ class ControllerExtensionModuleCategory extends Controller {
 
 		$this->load->model('catalog/product');
 
-		$this->load->model('catalog/category');
+		# $this->load->model('catalog/category');
 
 		$data['filters'] = $this->model_catalog_category->getAllFilters($i_root_id);
 
@@ -71,13 +71,33 @@ class ControllerExtensionModuleCategory extends Controller {
 		}
 
 		$data['lang'] = $this->language->get('code');
-		$aCategoryTree = $this->load->controller('common/category_tree', ['category_id' => $i_root_id]);
-		$data['category_tree'] = $aCategoryTree['tree'];
+		
+		# $aCategoryTree = $this->load->controller('common/category_tree', ['category_id' => $i_root_id]);
+		# $data['category_tree'] = $aCategoryTree['tree'];
 
 		//$aCategoryTree2 = $this->load->controller('common/category_tree/tree_type2', ['category_id' => 0]);
 		//$data['category_tree2'] = $aCategoryTree2['tree'];
 		// var_dump($data['category_tree2']); die();
-		file_put_contents('category_data.json', json_encode($data) );
+
+		#file_put_contents('category_data.json', json_encode([$parts, $data['categories']]) );
+
+		# category without chields
+		if( count($data['categories']) == 0 ){
+			$a_category = $this->model_catalog_category->getCategory( $parts[ count($parts)-1 ] );
+			# file_put_contents('category_data.json', json_encode($a_category) );	
+			$data['categories'][] = array(
+				'category_id' => $parts[ count($parts)-1 ],
+				'cat_path' => '',
+				'name' => $a_category['name'],
+				'children' => null,
+				'href' => '',
+				'top' => $a_category['top'],
+				'image' => $a_category['image'],
+				'class' => '', # 'background-color:rgb(0, 150, 69);'
+			);
+		}
+
+
 		return $this->load->view('extension/module/category', $data);
 	}
 }
